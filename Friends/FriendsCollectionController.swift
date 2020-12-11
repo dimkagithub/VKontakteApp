792ca747ce9bc5fb21a6450bc31917f127351ad6
Dmitry <dimkamail@gmail.com>
2020-12-11 15:06:40 +0500

    Added pictures, some logic

---
+++ Friends/FriendsCollectionController.swift
@@ -0,0 +1,63 @@
+//
+//  FriendsCollectionController.swift
+//  VKontakteApp
+//
+//  Created by Дмитрий on 10.12.2020.
+//
+
+import UIKit
+
+class FriendsCollectionController: UICollectionViewController {
+    
+    var images = [UIImage]()
+    var names = [String]()
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FriendCell", for: indexPath) as? FriendCell
+        else { return UICollectionViewCell() }
+        cell.userImage.image = images[indexPath.row]
+        cell.userName.text = names[indexPath.row]
+        
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
---
+++ Model/Users.swift
@@ -0,0 +1,24 @@
+//
+//  Users.swift
+//  VKontakteApp
+//
+//  Created by Дмитрий on 11.12.2020.
+//
+
+import UIKit
+
+struct Users {
+    let name: String
+    let image: UIImage?
+}
+
+let dale = Users(name: "Дейл", image: UIImage(named: "u1"))
+let tom = Users(name: "Том", image: UIImage(named: "u2"))
+let jerry = Users(name: "Джерри", image: UIImage(named: "u3"))
+let chip = Users(name: "Чип", image: UIImage(named: "u4"))
+let bunny = Users(name: "Заяц", image: UIImage(named: "u5"))
+let wolf = Users(name: "Волчара", image: UIImage(named: "u6"))
+let pup = Users(name: "Волк", image: UIImage(named: "u7"))
+let leupold = Users(name: "Леопольд", image: UIImage(named: "u8"))
+let wallie = Users(name: "Валл-И", image: UIImage(named: "u9"))
+let hackwrench = Users(name: "Гайка", image: UIImage(named: "u10"))
